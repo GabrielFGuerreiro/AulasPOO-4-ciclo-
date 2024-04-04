e4e7b68e52ecae0bb6a4cb5436ed1491c2893d7d
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-04-04 16:22:41 -0300

    Alterações exercícios plano de ensino

--- Projeto_Fatec/src/Exercicios/Plano_consulta.java
+++ Projeto_Fatec/src/Exercicios/Plano_consulta.java
@@ -2,12 +2,28 @@ package Exercicios;
 
 public class Plano_consulta
 {
-    public static void main(String args[])
+    public static void  main(String[] args)
     {
-        Plano_de_ensino a = new Plano_de_ensino();
-        a.mostradados();
+        Plano_de_ensino plano1 = new Plano_de_ensino();
 
-        Plano_de_ensino b = new Plano_de_ensino("Java","Método construtor", "A1 e A2", "Autor1");
-        b.mostradados();
+        plano1.setMat("Matéria1");
+        plano1.setCont("Conteúdo1");
+        plano1.setAva("A1+A2");
+        plano1.setBibli("Autor1");
+
+        System.out.println(plano1.getMat()); 
+        System.out.println(plano1.getCont());
+        System.out.println(plano1.getAva());
+        System.out.println(plano1.getBibli());
+
+     
+        Plano_de_ensino plano2 = new Plano_de_ensino("Matéria2","Conteúdo2","T1 e A2","Autor 2");
+
+       
+        System.out.println(plano2.getMat()); 
+        System.out.println(plano2.getCont());
+        System.out.println(plano2.getAva());
+        System.out.println(plano2.getBibli());
     }
 }
+
--- Projeto_Fatec/src/Exercicios/Plano_de_ensino.java
+++ Projeto_Fatec/src/Exercicios/Plano_de_ensino.java
@@ -1,36 +1,42 @@
+/* Criar o objeto “Plano de Ensino” com os atributos: matéria,
+conteúdo programático, critério de avaliação e bibliografia;
+2. Criar a versão padrão do método construtor;
+3. Criar uma versão do método construtor onde os atributos possam ser inicializados com valores;
+4. Criar os respectivos métodos sets( ) e gets( ) para cada atributo;
+5. Criar aplicação executável (classe main) e criar variáveis de instância para
+inserir os valores dos atributos através do método construtor e pelos métodos sets( );
+6. Retornar os valores inseridos pelos métodos gets( ), mostrando-os no console */
+
 package Exercicios;
 
 public class Plano_de_ensino
 {
-    private String materia;
-    private String cont_prog;
-    private String crit_ava;
-    private String bibli;
+    private String mat,cont,ava,bibli;
 
+    //1.1. Recebe os valores padrões
     Plano_de_ensino(){}
 
-    Plano_de_ensino(String materia,String cont_prog,String crit_ava,String bibli)
+    //construtor=set's
+    //2.1. Recebendo os valores do plano 2 pelos parâmetros E atribuindo as vars
+    Plano_de_ensino(String mat, String cont, String ava, String bibli)
     {
-        this.materia=materia;
-        this.cont_prog=cont_prog;
-        this.crit_ava=crit_ava;
+        this.mat=mat;
+        this.cont=cont;
+        this.ava=ava;
         this.bibli=bibli;
     }
 
-    public void setmateria(String materia) {this.materia=materia;}
-    public void setcont_prog(String cont_prog) {this.cont_prog=cont_prog;}
-    public void setcrit_ava(String crit_ava) {this.crit_ava=crit_ava;}
-    public void setbibli(String bibli) {this.bibli=bibli;}
+    //1.3. Recebendo os valores do plano 1 pelos parâmetros E atribuindo as vars
+    public void setMat(String mat) {this.mat=mat;}
+    public void setCont(String cont) {this.cont=cont;}
+    public void setAva(String ava) {this.ava=ava;}
+    public void setBibli(String bibli) {this.bibli=bibli;}
 
-    public String getMateria() {return materia;}
-    public String getcont_prog() {return cont_prog;}
-    public String getcrit_ava() {return crit_ava;}
-    public String getbibli() {return bibli;}
-
-    public void mostradados()
-    {
-        System.out.println("\n"+"materia:" + "\t" + materia + "\n"
-        + "cont_prog" + "\t" + cont_prog + "\n" + "crit_ava:" + "\t" + crit_ava + "\n"
-        + "bibli" + "\t" + bibli + "\n");
-    }
+    //1.4. Retornando os valores das variáveis do plano 1
+    //2.2. Retornando os valores das variáveis do plano 2
+    public String getMat() {return mat;}
+    public String getCont() {return cont;}
+    public String getAva() {return ava;}
+    public String getBibli() {return bibli;}
 }
+
